ee22c98e826d2f85ece65e4a762dac842f73ecbf
amishra1132000 <40968240-amishra1132000@users.noreply.replit.com>
2025-03-29 07:02:08 +0000

    Initial commit

---
+++ categories.py
@@ -0,0 +1,6 @@
+all_categories = {
+  "Christmas Songs": ["Jingle Bells", "Silent Night", "Deck the Halls", "White Christmas"],
+  "Christmas Films": ["Home Alone", "Elf", "The Grinch", "Love Actually"],
+  "Christmas Decorations": ["Ornaments", "Tinsel", "Wreath", "Stockings"],
+  "Christmas Foods": ["Turkey", "Gingerbread", "Eggnog", "Candy Canes"],
+}
